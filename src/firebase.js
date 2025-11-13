169865567d6834583fcdf61f1a6a44f7bbff16d4
semyhist <semih.aydin142@gmail.com>
2025-11-13 15:20:28 +0300

    v 0.1.1 deploy settings netlify.toml

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyCyvRoBBvMwHzRY4eDF5cySQmuQhZBAPgc",
+  authDomain: "trudevamsizlik.firebaseapp.com",
+  projectId: "trudevamsizlik",
+  storageBucket: "trudevamsizlik.firebasestorage.app",
+  messagingSenderId: "1093027829779",
+  appId: "1:1093027829779:web:b5cb03992ecb7c8f2147e6",
+  measurementId: "G-TDQVY3C6Y2"
 };
 
 const app = initializeApp(firebaseConfig);
